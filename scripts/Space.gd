69c179728d9694071f1afa59caaf77380715b45c
Ryuzaki <ufrshubham@gmail.com>
2020-06-02 20:12:12 +0530

    Partly added boundaries

--- scripts/Space.gd
+++ scripts/Space.gd
@@ -2,6 +2,10 @@ extends Node2D
 
 var missile = preload("res://scenes/Missile.tscn")
 var emeny = preload("res://scenes/Emeny.tscn")
+onready var window_dim = get_viewport_rect().size
+
+func _ready():
+	get_node("Player").set_position(window_dim/2)
 
 func _input(event):
 	if event.is_action_pressed("Fire"):
@@ -14,8 +18,8 @@ func _input(event):
 
 func _on_Timer_timeout():
 	var new_enemy = emeny.instance()
-	var x = randi() % 640
-	var y = randi() % 360
+	var x = randi() % int(window_dim.x)
+	var y = randi() % int(window_dim.y)
 	var rot = randi() % 360
 	new_enemy.set_position(Vector2(x, y))
 	new_enemy.set_rotation(deg2rad(rot))
